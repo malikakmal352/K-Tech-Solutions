2106ca52a8a21976991110b7a49b1bae908c75b1
Malik352 <malikakmalnaseer@gmail.com>
2021-12-11 23:24:12 +0500

    first commit

--- K_Tech/settings.py
+++ K_Tech/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=9qlg+%9u_^=^rx(!w5fo2t(k1v2p#es%&d4t*t7#*easpk#-f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG =True
 
 ALLOWED_HOSTS = []
 
