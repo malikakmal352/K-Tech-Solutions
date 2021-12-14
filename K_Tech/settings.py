6d9efcc954578105e393068eb7a1c2d8d81d1aaf
MALIK AKMAL Naseer <86542449+malikakmal352@users.noreply.github.com>
2021-12-15 01:39:49 +0500

    Update settings.py

--- K_Tech/settings.py
+++ K_Tech/settings.py
@@ -26,9 +26,9 @@ SECRET_KEY = 'django-insecure-=9qlg+%9u_^=^rx(!w5fo2t(k1v2p#es%&d4t*t7#*easpk#-f
 DEBUG =True
 
 ALLOWED_HOSTS = [
-   '/'
-   '127.0.0.1'
-   'k-tech-solutions.herokuapp.com'
+#    '/'
+#    '127.0.0.1'
+#    'k-tech-solutions.herokuapp.com'
 ]
 
 
