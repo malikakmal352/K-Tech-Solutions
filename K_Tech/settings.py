fb37419169c1f2fd2d7cda0245facd0f67492898
MALIK AKMAL Naseer <86542449+malikakmal352@users.noreply.github.com>
2021-12-25 15:55:20 +0500

    Update settings.py

--- K_Tech/settings.py
+++ K_Tech/settings.py
@@ -130,7 +130,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'Media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/Media/'
 
 STATICFILES_DIRS = [
